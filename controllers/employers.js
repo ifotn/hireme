1ca41eb4009cf51dc1b1b9d42dab0b80ee976a9c
ifotn <rich@infrontofthenet.com>
2023-02-03 17:39:33 -0500

    l4/2 - finish hireme setup

--- controllers/employers.js
+++ controllers/employers.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+// add fs to read the data file
+const fs = require('fs');
+
 /* GET employers index (the module home page) */
 router.get('/', (req, res) => {
+    const employers = [
+        {
+            "name": "Provix"
+        },
+        {
+            "name": "Element6"
+        },
+        {
+            "name": "Netgain"
+        },
+        {
+            "name": "44 North"
+        }
+    ];
+
     res.render('employers/index', {
-        title: 'Employer List'
+        title: 'Employer List',
+        employers: employers
     });
+    
+    // get data from json file
+    /*fs.readFile('./data/employers.json', 'utf8', (err, employers) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log(employers);
+            res.render('employers/index', {
+                title: 'Employer List',
+                employers: employers
+            });
+        }
+    });  */ 
 })
 
 // make public
