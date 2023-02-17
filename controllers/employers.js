1f8905fb5857104f313fbec48ec6c9aa19f11b2e
ifotn <rich@infrontofthenet.com>
2023-02-17 16:23:29 -0500

    l6/2 - add city dropdown, employers table

--- controllers/employers.js
+++ controllers/employers.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // add fs to read the data file
-const fs = require('fs');
+//const fs = require('fs');
 
 // use Employer model for CRUD w/mongoose
 const Employer = require('../models/employer');
+const City = require('../models/city');
 
 /* GET employers index (the module home page) */
 router.get('/', (req, res) => {
@@ -30,7 +31,7 @@ router.get('/', (req, res) => {
     });*/
 
     // get data from json file
-    fs.readFile('./data/employers.json', 'utf8', (err, employers) => {
+    /*fs.readFile('./data/employers.json', 'utf8', (err, employers) => {
         if (err) {
             console.log(err)
         }
@@ -41,12 +42,36 @@ router.get('/', (req, res) => {
                 employers: JSON.parse(employers)
             });
         }
-    }); 
+    }); */
+
+    // get data from mongodb using the Employer model
+    Employer.find((err, employers) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log(employers);
+            res.render('employers/index', {
+                title: 'Employer List',
+                employers: employers
+            });
+        }
+    });
 });
 
 /* GET /create - display form to add an employer */
 router.get('/create', (req, res) => {
-    res.render('employers/create');
+    // use City model to fetch list of cities from db to populate city dropdown
+    City.find((err, cities) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('employers/create', {
+                cities: cities
+            });
+        }
+    }).sort('name');    
 });
 
 /* POST /create - submit form data to mongodb */
